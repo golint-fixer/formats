3f0e5c315a305ebb5e960719aa12fd2da85533c7
mewmew <rnd0x00@gmail.com>
2016-03-27 03:05:36 +0200

    level/min: Add min package which implements access to MIN files.

---
+++ level/min/min.go
@@ -0,0 +1,146 @@
+// Package min implements access to MIN files.
+//
+// The MIN file of each level (e.g. "levels/l1data/l1.min") specifies how to
+// arrange the frames of the level's CEL file (e.g. "levels/l1data/l1.cel") in
+// order to form miniature tiles (henceforth referred to as dungeon pieces, or
+// pieces for short). A dungeon piece consists of either 10 or 16 blocks, where
+// each non-empty block is represented by a CEL frame.
+//
+// Below follows a pseudo-code description of the MIN file format.
+//
+//    // A MIN file consists of a sequence of dungeon piece definitions.
+//    type MIN []Piece
+//
+//    // A Piece consists of a sequence of either 10 or 16 block definitions.
+//    type Piece struct {
+//       // nblocks is either 10 (for "l1.min", "l2.min" and "l3.min") or 16
+//       // (for "l4.min" and "town.min").
+//       blocks [nblocks]Block
+//    }
+//
+//    // A Block stores the CEL frame number and frame type of a dungeon piece
+//    // block.
+//    //
+//    //    frameNum  := block&0x0FFF
+//    //    frameType := block&0x7000 >> 12
+//    type Block uint16
+package min
+
+import (
+	"bufio"
+	"encoding/binary"
+	"image"
+	"image/draw"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/mewkiz/pkg/errutil"
+)
+
+// A Piece represents a dungeon piece, which specifies how to arrange frames of
+// a level CEL file in order to form a miniature tile. A dungeon piece consists
+// of 10 or 16 blocks, where each non-empty block is represented by a CEL frame.
+type Piece struct {
+	// Either 10 or 16 blocks constituting the dungeon piece.
+	Blocks []Block
+}
+
+// A Block specifies the graphics of a single block in a dungeon piece,
+// consisting of either 10 or 16 blocks.
+type Block struct {
+	// Frame number in the level CEL file; or 0 if empty.
+	FrameNum int
+	// Frame type, specifying the CEL decoding algorithm of the frame.
+	FrameType int
+}
+
+// Parse parses the given MIN file and returns its dungeon piece definitions.
+func Parse(path string) ([]Piece, error) {
+	// Open file for reading.
+	fr, err := os.Open(path)
+	if err != nil {
+		return nil, errutil.Err(err)
+	}
+	defer fr.Close()
+	br := bufio.NewReader(fr)
+
+	// Allocate block buffer.
+	var nblocks int
+	switch name := filepath.Base(path); name {
+	case "l1.min", "l2.min", "l3.min":
+		nblocks = 10
+	case "l4.min", "town.min":
+		nblocks = 16
+	default:
+		return nil, errutil.Newf("min.Parse: support for %q not yet implemented", name)
+	}
+	buf := make([]uint16, nblocks)
+
+	// Decode dungeon pieces.
+	var pieces []Piece
+	for {
+		if err := binary.Read(br, binary.LittleEndian, buf); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, errutil.Err(err)
+		}
+		piece := Piece{
+			Blocks: make([]Block, nblocks),
+		}
+		for i := range buf {
+			block := Block{
+				FrameNum:  int(buf[i] & 0x0FFF),
+				FrameType: int(buf[i] & 0x7000 >> 12),
+			}
+			piece.Blocks[i] = block
+		}
+		pieces = append(pieces, piece)
+	}
+
+	return pieces, nil
+}
+
+// Image returns an image representation of the dungeon piece, where each non-
+// empty block corresponds to a CEL frame from levelFrames.
+//
+// The size of each block is 32x32 pixels and the blocks are arranged as
+// illustrated below to form a miniature tile.
+//
+//    +----+----+
+//    |  0 |  1 |
+//    +----+----+
+//    |  2 |  3 |
+//    +----+----+
+//    |  4 |  5 |
+//    +----+----+
+//    |  6 |  7 |
+//    +----+----+
+//    |  8 |  9 |
+//    +----+----+
+//    | 10 | 11 |
+//    +----+----+
+//    | 12 | 13 |
+//    +----+----+
+//    | 14 | 15 |
+//    +----+----+
+func (piece Piece) Image(levelFrames []image.Image) image.Image {
+	const (
+		blockWidth  = 32
+		blockHeight = 32
+	)
+	width := blockWidth * 2
+	height := blockHeight * (len(piece.Blocks) / 2)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for blockNum, block := range piece.Blocks {
+		if block.FrameNum != 0 {
+			frame := levelFrames[block.FrameNum-1]
+			x := blockWidth * (blockNum % 2)
+			y := blockHeight * (blockNum / 2)
+			dr := image.Rect(x, y, x+blockWidth, y+blockHeight)
+			draw.Draw(img, dr, frame, image.ZP, draw.Src)
+		}
+	}
+	return img
+}
